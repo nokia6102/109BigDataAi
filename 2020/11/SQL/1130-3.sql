c994b06d9d67977a440e65b2026bdeee3f03acb1
nokia6102 <nokia6102@gmail.com>
2020-11-30 14:52:01 +0800

    update

---
+++ 2020/11/SQL/1130-3.sql
@@ -0,0 +1,32 @@
+CREATE TABLE 家
+(
+    絪腹 INT IDENTITY(1,1) PRIMARY KEY,
+	家嘿 NVARCHAR(20),
+	家 VARBINARY(MAX)
+)
+GO
+
+ALTER PROC #TempPP @sqlQuery NVARCHAR(MAX),@trainedModel VARBINARY(MAX) OUTPUT
+AS
+	EXECUTE sp_execute_external_script @language = N'R',
+	@script = N'
+			inputData<-data.frame(InputDataSet)
+			cs<-colnames(inputData)
+			frm<-paste(cs[1],paste(cs[2:lenght(cs)],collapse="+",sep="~")
+			model<-rxLinMode(frm,inpuData)
+			print(model)
+			trainedModel<-rxSerializeMode(mode,realtimeScoringOnly=TRUE)
+			'
+			,@input_data_1=@sqlQuery
+			,@params=N'@trainedModel VARBINARY(MAX) OUTPUT'
+			,@trainedModel=@trainedModel OUTPUT
+;
+GO
+
+DECLARE @ss NVARCHAR(1024)=N'SELECT * FROM NewPitcher';
+DECLARE @mm VARBINARY(MAX);
+EXEC #TempPP @ss,@mm OUTPUT;
+INSERT INTO 家(家嘿,家) VALUES(N'絬┦癹耴()',@mm);
+
+-- truncate TABLE 家
+SELECT * FROM 家
