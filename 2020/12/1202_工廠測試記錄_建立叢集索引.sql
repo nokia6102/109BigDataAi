85888a928b0f281a87a997d55e2561ba3cb97103
nokia6102 <nokia6102@gmail.com>
2020-12-02 10:03:08 +0800

    update

--- 2020/12/1202_工廠測試記錄_建立叢集索引.sql
+++ 2020/12/1202_工廠測試記錄_建立叢集索引.sql
@@ -5,4 +5,8 @@ SELECT TOP (10000) *FROM [ex].[dbo].[telemetry]
 --https://docs.microsoft.com/zh-tw/sql/relational-databases/indexes/create-clustered-indexes?view=sql-server-ver15
 --建立一叢集索引 先用 machneID正序升冪排序, 再用datime正序升冪排序
 CREATE CLUSTERED INDEX CI ON [dbo].[telemetry] ([machineID] ASC, [datetime] ASC);
-SELECT TOP (10000) * FROM [ex].[dbo].[telemetry]
+
+
+SELECT TOP (10000) *, DATEPART(HOUR, datetime) AS H FROM [ex].[dbo].[telemetry] 
+WHERE ([datetime] >= '20150101 00:00:00' AND [datetime] <= '20150101 23:59:29') AND machineID=1
+ 
