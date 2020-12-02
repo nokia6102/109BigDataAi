612cbc020e1468172e77c91906aa67a406063e33
nokia6102 <nokia6102@gmail.com>
2020-12-02 09:50:08 +0800

    update

--- 2020/12/1202_工廠測試記錄_建立叢集索引.sql
+++ 2020/12/1202_工廠測試記錄_建立叢集索引.sql
@@ -1,7 +1,8 @@
 /****** SSMS 中 SelectTopNRows 命令的指令碼  ******/
  
-SELECT TOP (10000) *
-FROM [ex].[dbo].[telemetry]
+SELECT TOP (10000) *FROM [ex].[dbo].[telemetry]
 
 --https://docs.microsoft.com/zh-tw/sql/relational-databases/indexes/create-clustered-indexes?view=sql-server-ver15
+--建立一叢集索引 先用 machneID正序升冪排序, 再用datime正序升冪排序
 CREATE CLUSTERED INDEX CI ON [dbo].[telemetry] ([machineID] ASC, [datetime] ASC);
+SELECT TOP (10000) * FROM [ex].[dbo].[telemetry]
