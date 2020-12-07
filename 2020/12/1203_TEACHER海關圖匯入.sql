c3cf8e26fbedcde7593104dd7eecd614d4458df2
- <->
2020-12-07 13:36:07 +0800

    1203 and By人 寫給SQL呼叫的.NET C# CLR 計算出生肖

--- 2020/12/1203_TEACHER海關圖匯入.sql
+++ 2020/12/1203_TEACHER海關圖匯入.sql
@@ -7,6 +7,7 @@ AS
 	SELECT DISTINCT [Type] FROM [PicPathData]
 )
 SELECT IDENTITY(INT,1,1) AS Id,[Type]
+--DROP TABLE  [ObjectType]
 INTO [ObjectType]
 FROM Temp;
 
@@ -47,6 +48,7 @@ AS
 	FROM T3 AS A JOIN T2 AS B ON A.[Type]=B.[Type]
 	WHERE A.SerId<=B.Range
 )
+--DROP TABLE TrainPic
 SELECT * INTO TrainPic FROM T4;
 
 ---後3成做測試---
@@ -57,14 +59,118 @@ AS
 	EXCEPT
 	SELECT * FROM [TrainPic]
 )
+--DROP TABLE TestPic
 SELECT * INTO TestPic FROM Temp;
+
+
+--DROP PROC #TempPP
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+CREATE PROC #TempPP @sqlQuery NVARCHAR(MAX),@model VARBINARY(MAX) OUTPUT
+AS
+	EXECUTE sp_execute_external_script @language = N'R'
+        ,@script = N' 
+		sqlData<-data.frame(InputDataSet,stringsAsFactors = FALSE);
+		sqlData[] <- lapply(sqlData, function(x) if (is.factor(x)) as.character(x) else {x})
+		imageModel <- rxLogisticRegression( 
+			formula = Label~Features
+			,data = sqlData, 
+			,type = "multiClass", 
+			,mlTransforms = list( 
+				loadImage(vars = list(Features = "Pic")), 
+				resizeImage(vars = "Features", width = 224, height = 224), 
+				extractPixels(vars = "Features"), 
+				featurizeImage(var = "Features", dnnModel = "Resnet101"))
+			,mlTransformVars = "Pic");	
+		 model <- rxSerializeModel(imageModel, realtimeScoringOnly = FALSE);
+		 print(model)
+		'
+		,@input_data_1 = @sqlQuery
+		,@params = N'@model VARBINARY(MAX) OUTPUT'
+		,@model = @model OUTPUT;
+GO
+
+DECLARE @model VARBINARY(MAX);
+DECLARE @query nvarchar(max) = N'SELECT  [Pic],[Type],[Label] FROM TrainPic';
+EXEC #TempPP @query,@model OUTPUT;
+INSERT INTO 模型表(模型名稱,模型) VALUES(N'羅吉斯(圖型)',@model);
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+SELECT * FROM 模型表
+
+CREATE TABLE 模型表
+(
+    編號 INT IDENTITY(1,1) PRIMARY KEY,
+	模型名稱 NVARCHAR(20),
+	模型 VARBINARY(MAX),
+	建檔時間 DATETIME2(2) DEFAULT SYSDATETIME()
+)
+
+TRUNCATE TABLE  模型表
+SELECT  * FROM 模型表
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+DECLARE @savedmodel VARBINARY(MAX);
+SELECT @savedmodel=模型 FROM 模型表 WHERE 編號=6;
+
+DECLARE @query NVARCHAR(MAX);
+SET @query=N'SELECT TOP(3) * FROM TestPic ORDER BY NEWID()';
+
+EXEC sp_execute_external_script 
+  @language = N'R',
+  @script = N'
+    mod <- rxUnserializeModel(model);
+    
+	testdata<-data.frame(InputDataSet,stringsAsFactors = FALSE);
+	testdata[] <- lapply(testdata, function(x) if (is.factor(x)) as.character(x) else {x})
+	predict_result <- rxPredict(modelObject = mod, data = testdata,extraVarsToWrite = "Label");	
+		
+	#print(summary(predict_result));
+	#print(predict_result);
+
+	OutputDataSet <- predict_result;
+  '
+  ,@input_data_1 = @query
+  ,@params = N'@model varbinary(max)'
+  ,@model=@savedmodel
+GO
+
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+CREATE PROC EvalAnimal @PathInfo NVARCHAR(100)
+AS
+	CREATE TABLE #Test
+	(
+		[Pic] NVARCHAR(128),
+		[Type] NVARCHAR(128),
+		[Label] INT
+	);			
+	INSERT INTO #Test([Pic],[Type],[Label]) VALUES(@PathInfo,N'',0);
+		
+	CREATE TABLE #TempResult
+	(		
+		[PredictedLabel] INT,
+		Score1 FLOAT,Score2 FLOAT,Score3 FLOAT,
+		Score4 FLOAT,Score5 FLOAT,Score6 FLOAT,
+		Score7 FLOAT,Score8 FLOAT,Score9 FLOAT,
+		Score10 FLOAT,Score11 FLOAT,Score12 FLOAT,
+		Score13 FLOAT,Score14 FLOAT,Score15 FLOAT
+	);
+	
+	DECLARE @savedModel varbinary(max);
+	SELECT @savedModel = 模型 FROM 模型表_動物 WHERE 編號=3;
+	
+	INSERT INTO #TempResult
+	EXEC sp_rxPredict @model = @savedModel,@inputData = N'SELECT [Pic],[Label] FROM #Test';
+
+	SELECT A.Type,B.*
+	FROM AnimalType AS A JOIN #TempResult AS B ON A.Id=B.[PredictedLabel]
+GO
+
+EXEC EvalAnimal N'C:\PP\測試動物區\Mike.png';
+EXEC EvalAnimal N'C:\PP\測試動物區\KK.jpg';
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
